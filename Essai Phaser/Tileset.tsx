aba9f5dfa7eb725bdee67198e082cdad00bbb260
ClementBck <118446250+ClementBck@users.noreply.github.com>
2023-02-05 15:56:24 +0100

    Add files via upload

---
+++ Essai Phaser/Tileset.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="3136" columns="56">
+ <image source="Tileset.png" width="1800" height="1800"/>
+ <tile id="50">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
